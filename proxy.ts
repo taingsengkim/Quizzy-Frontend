cb60f0339df1ed6222d13715648a9d18baf20414
unknown <mrrsaki0403@gmail.com>
2026-04-08 14:06:21 +0700

    Proxy route

---
+++ proxy.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function proxy(request: NextRequest) {
+const token = request.cookies.get("better-auth.session_data")?.value;
+
+  const isProtectedRoute = request.nextUrl.pathname.startsWith("/quizzes/play");
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+ 
+export const config = {
+  matcher: ["/quizzes/play/:path*"],
+}
