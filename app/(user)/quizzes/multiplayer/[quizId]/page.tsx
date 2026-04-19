4540c1adde33ec45ac2b6ccdc6fb59b785d49fa7
unknown <mrrsaki0403@gmail.com>
2026-04-19 13:47:26 +0700

    Websocket Fix( will handle route later )

--- app/(user)/quizzes/multiplayer/[quizId]/page.tsx
+++ app/(user)/quizzes/multiplayer/[quizId]/page.tsx
@@ -8,9 +8,12 @@ export default async function MultiPlayerPage({
 }) {
   const { quizId } = await params;
 
-  const res = await fetch(`http://localhost:8090/api/v1/quizzes/${quizId}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://quizzy-springboot-1.onrender.com/api/v1/quizzes/${quizId}`,
+    {
+      cache: "no-store",
+    },
+  );
 
   if (!res.ok) {
     return <NotFoundQuiz />;
