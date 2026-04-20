e9f8de581a45cab00cf42e340a504c2b6f7f944f
unknown <mrrsaki0403@gmail.com>
2026-04-20 21:14:06 +0700

    Add Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,9 +100,9 @@ export const metadata: Metadata = {
   // --- Icons / Favicon ---
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon/favicon.ico" },
+      { url: "/icon/favicon-16x16.ico", sizes: "16x16", type: "image/png" },
+      { url: "/icon/favicon-32x32.ico", sizes: "32x32", type: "image/png" },
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
     shortcut: "/favicon.ico",
